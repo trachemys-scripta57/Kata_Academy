b1e6c05eddee85ee522337015d7050a4be8f3686
trachemys-scripta57 <102344315+trachemys-scripta57@users.noreply.github.com>
2022-07-06 10:33:43 +0300

    Add files via upload
    final version

--- arrayOddNumbers.java
+++ arrayOddNumbers.java
@@ -3,14 +3,20 @@ public class arrayOddNumbers {
         StringBuilder a = new StringBuilder();
         boolean b = true;
         for (int c : arr) {
-            if (Math.abs(c % 2) == 1)
+            if (Math.abs(c % 2) == 1) {
                 if (b) {
                     a.append(c);
                     b = false;
                 } else {
-                    a.append(", ").append(c);
+                    a.append(",").append(c);
                 }
+            }
         }
         System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        int[] strArray = new int[]{1, 3, 15, 27, 19, -2, 0, 12, -3};
+        printOddNumbers(strArray);
     }
 }
